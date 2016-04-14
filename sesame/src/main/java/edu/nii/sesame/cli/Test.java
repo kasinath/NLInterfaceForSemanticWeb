4d5b2c1950d6d11be6173c0240e7aa6185b28f33
kasinathan <kasinathan>
2016-04-14 18:48:57 +0900

    intermediate commit

--- sesame/src/main/java/edu/nii/sesame/cli/Test.java
+++ sesame/src/main/java/edu/nii/sesame/cli/Test.java
@@ -11,35 +11,79 @@ public class Test {
 
     
 	public static void main(String[] args) {
-
-		List<String> l = new ArrayList<String>();
-		String q  =  "Which art movement did Picasso belong to";
-		l.add(q);
-		
-		q  =  "What art movement did Picasso and Raphael belong to";
-		l.add(q);
-		
-		q  =  "Did Picasso and Raphael belong to same movement?";
-		l.add(q);
+		//test_movement();
+		//test_geography();
+		//test_dates();
+		//test_influence();
+		test_Artwork();
 		
 		
-		q  =  "Did Picasso  belong to Expressionism?";
-		l.add(q);
-	
-		q  =  "Which artists were involved in  Renaissance movement?";
-		l.add(q);
-		
-		
-		
-		for(String  x : l)
-		{
-			System.out.println("\n");
-			TemplateCreation tc = new TemplateCreation(x);
-			tc.generateTemplate();	
-		}
 		
 				 
 		
 	    
+	}
+	private static void test_Artwork()
+	{
+		List<String> questions = new ArrayList<String>();
+		questions.add( "Which artist had the most number of art works");
+		questions.add( "List the top five artists with most number of art works");
+		questions.add("List the top five artists with least number of art works");
+
+		test(questions);				
+	}
+	private static void test_influence() {
+		List<String> questions = new ArrayList<String>();
+		questions.add( "Which artists did Picasso influence/inspire?");
+		questions.add( "Which artists were influenced by Picasso?");
+		questions.add("Which artists were influenced by Picasso?");
+
+		test(questions);		
+	}
+	private static void test_dates() {
+		List<String> questions = new ArrayList<String>();
+		questions.add( "Which artists were born in the 15th century");
+		questions.add( "Which artists were born in 1654");
+		questions.add("Which artists were born in 1650s");
+		questions.add("Which artists died in 1654");
+		questions.add("Which artists died in 1650s");
+		test(questions);
+		
+	}
+	public static void test_geography()
+	{
+		
+		List<String> questions = new ArrayList<String>();
+		questions.add( "Which artists came from France?");
+		questions.add( "Which Artists came from Picasso's hometown");
+		questions.add("Which Artists came from Picasso's hometown");
+		questions.add("Where did Picasso die");
+		questions.add("Did Picasso die in Europe?");
+		test(questions);
+		
+	}
+	
+	public static void test_movement()
+	{
+		
+		List<String> questions = new ArrayList<String>();
+		questions.add( "Which art movement did Picasso belong to");
+		questions.add( "What art movement did Picasso and Raphael belong to");
+		questions.add("Did Picasso and Raphael belong to same movement?");
+		questions.add("Did Picasso  belong to Expressionism?");
+		questions.add("Which artists were involved in  Renaissance movement?");
+		test(questions);
+		
+	}
+	
+	public static void test(List<String> questions)
+	{
+		for(String  question : questions)
+		{
+			System.out.println("\n");
+			TemplateCreation tc = new TemplateCreation(question);
+			tc.generateTemplate();	
+		}
+		
 	}
 }
